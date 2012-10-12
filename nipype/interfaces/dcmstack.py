5eb92227832d960697243b1d334324ac6ec1903b
Chris Filo Gorgolewski <chris.gorgolewski@gmail.com>
2012-10-12 10:29:52 +0200

    Removed EPI specific outputs

--- nipype/interfaces/dcmstack.py
+++ nipype/interfaces/dcmstack.py
@@ -9,8 +9,6 @@ class DCMStackInputSpec(TraitedSpec):
     
 class DCMStackOutputSpec(TraitedSpec):
     nifti_file = File(exists=True)
-    tr = traits.Float()
-    sliceorder = traits.List(traits.Int())
     
 class DCMStack(BaseInterface):
     input_spec = DCMStackInputSpec
@@ -24,15 +22,9 @@ class DCMStack(BaseInterface):
             my_stack.add_dcm(src_dcm)
         nii_wrp = my_stack.to_nifti_wrapper()
         nii_wrp.to_filename(os.path.abspath(self.inputs.out_filename))
-        self._sliceorder = np.argsort(nii_wrp.meta_ext.get_values('CsaImage.MosaicRefAcqTimes')[0]).tolist()
-        self._tr = nii_wrp.meta_ext.get_values('RepetitionTime')
-        if self._tr > 100:
-            self._tr = self._tr/1000.
         return runtime
 
     def _list_outputs(self):
         outputs = self._outputs().get()
         outputs["nifti_file"] = os.path.abspath(self.inputs.out_filename)
-        outputs["sliceorder"] = self._sliceorder
-        outputs["tr"] = self._tr
         return outputs
